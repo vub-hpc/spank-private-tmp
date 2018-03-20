7f2348ca3a7d39e54247c045536d1459205aa60f
Andy Georges <itkovian@ugent.be>
2018-03-20 14:18:57 +0100

    Adjusted spec file and added bdist_rpm.sh

---
+++ bdist_rpm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VERSION=$(grep "Version:.*[0-9]" slurm-spank-private-tmpdir.spec | tr -s " " |  awk '{print $2;}')
+RELEASE=$(grep "%global rel.*[-1-9]" slurm-spank-private-tmpdir.spec | tr -s " " | awk '{print $3}')
+
+if [ "${RELEASE}" -gt 1 ]; then
+    SUFFIX=${VERSION}-${RELEASE}
+else
+    SUFFIX=${VERSION}
+fi
+
+GITTAG=$(git log --format=%ct.%h -1)
+
+mkdir -p BUILD SOURCES SPECS RPMS BUILDROOT
+git archive --format=tar.gz -o "SOURCES/slurm-spank-private-tmpdir-${SUFFIX}.tar.gz" --prefix="slurm-spank-private-tmpdir-${SUFFIX}/" HEAD
+cp slurm-spank-private-tmpdir.spec "SPECS"
+rpmbuild --define "gittag ${GITTAG}" --define "_topdir $PWD" -ba SPECS/slurm-spank-private-tmpdir.spec
